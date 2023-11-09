738ca1c7bf72e55f04fd0e35b3694b2973119048
GalafassiNAT <natanaelgalafassintg@gmail.com>
2023-11-08 21:47:26 -0300

    Fix tree removal bug and add test cases

--- Meus exercicios/Tree/abb.hpp
+++ Meus exercicios/Tree/abb.hpp
@@ -55,11 +55,24 @@ public:
     }
 
     // remove o nó com a chave especificada
-    void remover(C chave);
+    void remover(C chave){
+        this->raiz = removerNo(this->raiz, chave);
+    }
     // imprime a árvore
     void imprimir();
     // retorna uma referência ao nó com a menor chave
     NoABB<C, V>* minimo();
+
+    NoABB<C, V>* minimo(NoABB<C,V>* no){
+        if(no == nullptr)
+            return nullptr;
+
+
+        if(no->esq== nullptr)
+            return no;
+        return minimo(no->esq);
+
+    }
     // retorna uma referência ao nó com a maior chave
     NoABB<C, V>* maximo();
     // retorna uma referência ao nó com a chave sucessora da chave especificada
@@ -82,6 +95,47 @@ private:
         else
             no->dir = inserirNo(no->dir, chave, valor);
         return no;
+    }
+
+    NoABB<C,V> *removerNo(NoABB<C,V>* no, C chave){
+        if(no == nullptr){
+            return nullptr;
+        }
+
+        if(chave < no->chave)
+            no->esq = removerNo(no->esq, chave);
+        else if(chave > no->chave)
+            no->dir = removerNo(no->dir, chave);
+        else{
+            //caso 1: O elemento a ser removido é folha
+            if(no->esq == nullptr && no->dir == nullptr){
+                delete no;
+                return nullptr;
+            }
+            //caso 2: O elemento a ser removido tem um filho
+            //2a: Ta falantando o filho esquerdo
+            if(no->esq == nullptr){
+                auto aux = no->dir;
+                delete no;
+                return aux;
+            }
+            //2b: Ta falantando o filho direito
+            if(no->dir == nullptr){
+                auto aux = no->esq;
+                delete no;
+                return aux;
+            }
+            // Caso 3: O nó removido tem 2 filho
+            // encontrar o sucessor
+            auto suc = minimo(no->dir);
+            no->chave = suc->chave;
+            no->valor = suc->valor;
+            no->dir = removerNo(no->dir, suc->chave);
+
+        }
+        
+        return no;
+
     }
 
     void imprimirNo(NoABB<C, V> *no, int nivel, char lado);
